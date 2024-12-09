3629269fecb1ae12ff05415ff8fc2db786c40a76
Aman24Raj <amanrajmathematics@gmail.com>
2024-12-09 18:40:10 +0530

    added tls

--- verifier/smtp.go
+++ verifier/smtp.go
@@ -6,10 +6,11 @@ import (
 	"fmt"
 	"net"
 	"net/textproto"
+	"crypto/tls"
 	"strings"
 )
 
-const smtpServer = "smtp.gmail.com:587"
+const smtpServer = "smtp.gmail.com:465"
 
 const sender_email = "amanrajmathematics@gmail.com"
 
@@ -27,6 +28,10 @@ func VerifyDomain(target_email string) (bool, error) {
 	// Defer till the end 
 	defer conn.Close()
 
+	conn = tls.Client(conn, &tls.Config{
+		InsecureSkipVerify : true, 
+	})
+
 	// Buffer reader initialization
 	bufferReader := bufio.NewReader(conn) 
 	tpReader := textproto.NewReader(bufferReader)
@@ -36,6 +41,33 @@ func VerifyDomain(target_email string) (bool, error) {
 	if _, err := tpReader.ReadLine(); err != nil {
 		return false, errors.New("ERR_READLINE_WELCOME")
 	}
+
+	// // TLS Service Initialize 
+	// if _, err := fmt.Fprintf(conn, "EHLO smtp.gmail.com\r\n"); err != nil {
+	// 	return false, errors.New("ERR_SENDING_EHLO")
+	// }
+
+	// // Server response
+	// if _, err = tpReader.ReadLine() ; err != nil {
+	// 	return false, errors.New("ERR_READING_EHLO_RESPONSE")
+	// }
+
+	// // Send STARTTLS command to start encryption
+	// if _, err := fmt.Fprintf(conn, "STARTTLS\r\n"); err != nil {
+	// 	return false, errors.New("ERR_SENDING_STARTTLS")
+	// }
+
+	// // Read the response to STARTTLS
+	// if response, err := tpReader.ReadLine() ; err != nil || !strings.HasPrefix(response, "220") {
+	// 	return false, errors.New("ERR_STARTTLS_RESPONSE")
+	// }
+
+	// // Upgrade connection to TLS (secure) connection
+	// conn = tls.Client(conn, &tls.Config{
+	// 	InsecureSkipVerify: true,
+	// })
+
+
 
 	// MAIL 
 	if _, err := fmt.Fprintf(conn, "MAIL FROM:<%s>\r\n", sender_email); err != nil {
@@ -54,6 +86,8 @@ func VerifyDomain(target_email string) (bool, error) {
 
 	// Server response
 	response, err := tpReader.ReadLine()
+
+	fmt.Println(response)
 
 	if err != nil {
 		return false, errors.New("ERR_READING_RCPT_RESPONSE")
