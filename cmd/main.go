c6b0d9246369a99c8ed33b3afd14a6a5f8b8f9bc
Aman24Raj <amanrajmathematics@gmail.com>
2024-12-10 15:00:17 +0530

    Added some polishing

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/amanfoundongithub/email_verifier/api"
 )
 
@@ -11,7 +9,7 @@ func main() {
 	// Create new server
 	server := api.CreateEmailServer()
 
-	//
-	http.ListenAndServe(":8080", server) 
+	// Activate the server 
+	api.ActivateServer(server, ":8080")
 
 }
